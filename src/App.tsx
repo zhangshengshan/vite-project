9d6018ded61281e79054f2ecb0707fac7dd63da7
zhangshengshan <zsszsszsszss@126.com>
2024-03-04 21:45:05 +0800

    first commit!

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import { Excalidraw  } from "@excalidraw/excalidraw";
+
+function App() {
+
+  return (
+    <>
+      {/*<h1>Excalidraw Example</h1>*/}
+      <div id="board" >
+        <Excalidraw />
+      </div>
+    </>
+  )
+}
+
+export default App
