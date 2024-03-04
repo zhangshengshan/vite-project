1efc46ef6e5927d56f162ccae5db807e9b7c5d25
zhangshengshan <zsszsszsszss@126.com>
2024-03-04 23:39:25 +0800

    first commit!

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,25 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': process.env
+  clearScreen: false,
+  define:{
+    'process.env': process.env,
+  },
+  // Tauri expects a fixed port, fail if that port is not available
+  server: {
+    strictPort: true,
+  },
+  // base: process.env.NODE_ENV === 'production' ? './' : '/',
+
+  // to access the Tauri environment variables set by the CLI with information about the current target
+  envPrefix: ['VITE_', 'TAURI_PLATFORM', 'TAURI_ARCH', 'TAURI_FAMILY', 'TAURI_PLATFORM_VERSION', 'TAURI_PLATFORM_TYPE', 'TAURI_DEBUG'],
+  build: {
+    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+    target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+    // don't minify for debug builds
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    // 为调试构建生成源代码映射 (sourcemap)
+    sourcemap: !!process.env.TAURI_DEBUG,
+    // assetsDir: 'assets',
   },
 })
