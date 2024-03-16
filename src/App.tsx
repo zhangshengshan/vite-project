3ab753939ef56063a7ae440b78f39bd049bb0840
zhangshengshan <zsszsszsszss@126.com>
2024-03-16 21:39:06 +0800

    add antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,36 @@
-
 import './App.css'
-import { Excalidraw  } from "@excalidraw/excalidraw";
-import { Button  } from 'antd';
+import {Excalidraw} from "@excalidraw/excalidraw";
+import {Button, Flex} from 'antd';
 
 
 function App() {
 
-  return (
-    <>
-      <div id="board" >
-        <Excalidraw />
-        <Button/>
-      </div>
-    </>
-  )
+    return (
+        <>
+            {/*<Flex vertical gap="small" style={{width: '100%'}}>*/}
+            {/*    <Button type="primary" block>*/}
+            {/*        Primary*/}
+            {/*    </Button>*/}
+            {/*    <Button block>Default</Button>*/}
+            {/*    <Button type="dashed" block>*/}
+            {/*        Dashed*/}
+            {/*    </Button>*/}
+            {/*    <Button disabled block>*/}
+            {/*        disabled*/}
+            {/*    </Button>*/}
+            {/*    <Button type="text" block>*/}
+            {/*        text*/}
+            {/*    </Button>*/}
+            {/*    <Button type="link" block>*/}
+            {/*        Link*/}
+            {/*    </Button>*/}
+            {/*</Flex>*/}
+            <div id="board">
+                <Excalidraw/>
+                <Button/>
+            </div>
+        </>
+    )
 }
 
 export default App
