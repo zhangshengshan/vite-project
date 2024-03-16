20e53dea0dc6dde80f9d3735680fb4f7f976abaf
zhangshengshan <zsszsszsszss@126.com>
2024-03-16 23:40:22 +0800

    add antd

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+// src/pages/HomePage.tsx
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <p>Welcome to the home page!</p>
+        </div>
+    );
+}
+
+export default HomePage;
---
+++ src/pages/UserPage.tsx
@@ -0,0 +1,12 @@
+// src/pages/UserPage.tsx
+
+const UserPage = () => {
+    return (
+        <div>
+            <h1>User Page</h1>
+            <p>Welcome to the user page!</p>
+        </div>
+    );
+}
+
+export default UserPage;
