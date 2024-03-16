4f8f7de1376f5bcd46514e5a9620b3b24925ca34
zhangshengshan <zsszsszsszss@126.com>
2024-03-17 00:26:39 +0800

    add antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { Nav, Avatar, Tabs, TabPane } from '@douyinfe/semi-ui';
-import { IconSemiLogo, IconFeishuLogo, IconHelpCircle, IconBell } from '@douyinfe/semi-icons';
-import { IconIntro, IconHeart, IconCalendar, IconCheckbox, IconRadio, IconList, IconToast } from '@douyinfe/semi-icons-lab';
+import {Nav, Avatar} from '@douyinfe/semi-ui';
+import {IconSemiLogo, IconFeishuLogo, IconHelpCircle, IconBell} from '@douyinfe/semi-icons';
+import {IconIntro, IconHeart, IconCalendar, IconRadio} from '@douyinfe/semi-icons-lab';
 import styles from './index.module.scss';
 import {Route, Routes, useNavigate} from 'react-router-dom';
 import HomePage from "./pages/HomePage.tsx";
 import UserPage from "./pages/UserPage.tsx";
 import DashBoardPage from "./pages/DashBoardPage.tsx";
+import ExternalPage from "./pages/ExternalPage.tsx";
 
 const App = () => {
     const navigate = useNavigate();
@@ -18,16 +18,16 @@ const App = () => {
                 header={{
                     logo: (
                         <div className={styles.navigationHeaderLogo}>
-                            <IconSemiLogo className={styles.semiIconsSemiLogo} />
+                            <IconSemiLogo className={styles.semiIconsSemiLogo}/>
                         </div>
                     ),
                     text: "Semi Templates",
                 }}
                 footer={
                     <div className={styles.dIV}>
-                        <IconFeishuLogo size="large" className={styles.semiIconsFeishuLogo} />
-                        <IconHelpCircle size="large" className={styles.semiIconsHelpCircle} />
-                        <IconBell size="large" className={styles.semiIconsBell} />
+                        <IconFeishuLogo size="large" className={styles.semiIconsFeishuLogo}/>
+                        <IconHelpCircle size="large" className={styles.semiIconsHelpCircle}/>
+                        <IconBell size="large" className={styles.semiIconsBell}/>
                         <Avatar
                             size="small"
                             src="https://sf6-cdn-tos.douyinstatic.com/obj/eden-cn/ptlz_zlp/ljhwZthlaukjlkulzlp/root-web-sites/avatarDemo.jpeg"
@@ -40,46 +40,48 @@ const App = () => {
                 }
                 className={styles.nav}
             >
-                <Nav.Item itemKey="Home" text="Home" onClick={() => navigate('/home')} />
-                <Nav.Item itemKey="Project" text="Project" onClick={()=> navigate("/project")} />
-                <Nav.Item itemKey="Board" text="Board" onClick={()=>navigate("/board")}/>
-                <Nav.Item itemKey="Forms" text="Forms" onClick={()=>navigate("/forms")}/>
+                <Nav.Item itemKey="Home" text="Home" onClick={() => navigate('/home')}/>
+                <Nav.Item itemKey="Project" text="Project" onClick={() => navigate("/project")}/>
+                <Nav.Item itemKey="Board" text="Board" onClick={() => navigate("/board")}/>
+                <Nav.Item itemKey="Forms" text="Forms" onClick={() => navigate("/forms")}/>
             </Nav>
             <div className={styles.main}>
                 <Nav
                     defaultOpenKeys={["user", "union"]}
-                    bodyStyle={{ height: 918 }}
+                    bodyStyle={{height: 918}}
                     mode="vertical"
-                    footer={{ collapseButton: true }}
+                    footer={{collapseButton: true}}
                     className={styles.left}
                 >
                     <Nav.Item
                         itemKey="Home"
                         text="Home"
-                        icon={<IconIntro className={styles.iconIntro} />}
+                        icon={<IconIntro className={styles.iconIntro}/>}
                         className={styles.navItem}
                         onClick={() => navigate('/home')}
                     />
                     <Nav.Item
                         itemKey="Dashboard"
                         text="Dashboard"
-                        icon={<IconHeart className={styles.iconHeart} />}
+                        icon={<IconHeart className={styles.iconHeart}/>}
                         className={styles.navItem1}
                         onClick={() => navigate('/dashboard')}
                     />
                     <Nav.Item
                         itemKey="Project"
                         text="Project"
-                        icon={<IconCalendar className={styles.iconCalendar} />}
+                        icon={<IconCalendar className={styles.iconCalendar}/>}
                         className={styles.navItem2}
                         onClick={() => navigate('/project')}
                     />
                     <Nav.Item
                         itemKey="Users"
                         text="Users"
-                        icon={<IconRadio className={styles.iconRadio} />}
+                        icon={<IconRadio className={styles.iconRadio}/>}
                         className={styles.navItem5}
-                        onClick={() => navigate('/users')}
+                        // onClick={() => navigate('/users')}
+
+                        onClick={() => window.open('http://www.baidu.com', '_blank')}
                     />
                 </Nav>
                 <div className={styles.mainRight}>
@@ -88,10 +90,10 @@ const App = () => {
                         <div className={styles.yourContent}>
                             <p className={styles.yourContentHere}>
                                 <Routes>
-                                    <Route path="/home" element={<HomePage />} />
-                                    <Route path="/dashboard" element={<DashBoardPage/>} />
-                                    <Route path="/forms" element={<UserPage/>} />
-                                    <Route path="/users" element={<UserPage/>} />
+                                    <Route path="/home" element={<HomePage/>}/>
+                                    <Route path="/dashboard" element={<DashBoardPage/>}/>
+                                    <Route path="/forms" element={<UserPage/>}/>
+                                    <Route path="/users" element={<ExternalPage/>}/>
                                 </Routes>
                             </p>
                         </div>
@@ -102,4 +104,4 @@ const App = () => {
     );
 }
 
-export default App ;
+export default App;
---
+++ src/pages/ExternalPage.tsx
@@ -0,0 +1,7 @@
+const ExternalPage = () => {
+    return (
+        <iframe src="http://www.baidu.com" style={{width: '100%', height: '100%'}} title="External Page"></iframe>
+    );
+};
+
+export default ExternalPage;
