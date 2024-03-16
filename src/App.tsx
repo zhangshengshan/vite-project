cdc31ff05e317f48565d17beaae8e5b6c49c6368
zhangshengshan <zsszsszsszss@126.com>
2024-03-16 21:32:46 +0800

    add antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 
 import './App.css'
 import { Excalidraw  } from "@excalidraw/excalidraw";
+import { Button  } from 'antd';
+
 
 function App() {
 
@@ -8,6 +10,7 @@ function App() {
     <>
       <div id="board" >
         <Excalidraw />
+        <Button/>
       </div>
     </>
   )
