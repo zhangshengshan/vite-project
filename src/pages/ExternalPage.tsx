07d505eca8c6bde6895d992baaf9029981dace37
zhangshengshan <zsszsszsszss@126.com>
2024-03-17 00:35:12 +0800

    add antd

--- src/pages/ExternalPage.tsx
+++ src/pages/ExternalPage.tsx
@@ -1,6 +1,6 @@
 const ExternalPage = () => {
     return (
-        <iframe src="http://www.baidu.com" style={{width: '100%', height: '100%'}} title="External Page"></iframe>
+        <iframe src="https://github.com" style={{width: '100%', height: '100%'}} title="External Page"></iframe>
     );
 };
 
