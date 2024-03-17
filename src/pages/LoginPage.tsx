29efacdb38fba7a86438bdee424ea29f08aa8f09
zhangshengshan <zsszsszsszss@126.com>
2024-03-17 21:25:28 +0800

    add antd

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,11 +1,11 @@
 import {useState} from 'react';
-import {useNavigate} from 'react-router-dom';
+// import {useNavigate} from 'react-router-dom';
 import {Notification, Input, Button} from '@douyinfe/semi-ui';
 import styles from './LoginPage.module.scss';
 import Label from "@douyinfe/semi-ui/lib/es/form/label";
 
 const LoginPage = ({setIsLoggedIn, onLoginSuccess}) => {
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
