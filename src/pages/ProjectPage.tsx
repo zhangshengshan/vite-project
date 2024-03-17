736769478a1e44ae86f153aa13ee1423cacce665
zhangshengshan <zsszsszsszss@126.com>
2024-03-17 21:44:58 +0800

    add antd

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -1,61 +1,7 @@
 import {Divider, Table} from "@douyinfe/semi-ui";
+import {columns, data} from "../mock/mockData.tsx";
 
 const ProjectPage = () => {
-    const columns = [
-        {
-            title: '标题',
-            dataIndex: 'name',
-        },
-        {
-            title: '大小',
-            dataIndex: 'size',
-        },
-        {
-            title: '所有者',
-            dataIndex: 'owner',
-        },
-        {
-            title: '更新日期',
-            dataIndex: 'updateTime',
-        },
-
-        {
-            title: 'icon',
-            dataIndex: 'nameIconSrc',
-            render: (text, record) => <img src={record.nameIconSrc} alt={record.name}
-                                           style={{width: '20px', height: '20px'}}/>
-        }
-
-    ];
-    const data = [
-        {
-            key: '1',
-            name: 'Semi Design 设计稿.fig',
-            nameIconSrc: 'https://lf3-static.bytednsdoc.com/obj/eden-cn/ptlz_zlp/ljhwZthlaukjlkulzlp/figma-icon.png',
-            size: '2M',
-            owner: '姜鹏志',
-            updateTime: '2020-02-02 05:13',
-            avatarBg: 'grey',
-        },
-        {
-            key: '2',
-            name: 'Semi Design 分享演示文稿',
-            nameIconSrc: 'https://lf3-static.bytednsdoc.com/obj/eden-cn/ptlz_zlp/ljhwZthlaukjlkulzlp/docs-icon.png',
-            size: '2M',
-            owner: '郝宣',
-            updateTime: '2020-01-17 05:31',
-            avatarBg: 'red',
-        },
-        {
-            key: '3',
-            name: '设计文档',
-            nameIconSrc: 'https://lf3-static.bytednsdoc.com/obj/eden-cn/ptlz_zlp/ljhwZthlaukjlkulzlp/docs-icon.png',
-            size: '34KB',
-            owner: 'Zoey Edwards',
-            updateTime: '2020-01-26 11:01',
-            avatarBg: 'light-blue',
-        },
-    ];
 
     return (
         <div>
