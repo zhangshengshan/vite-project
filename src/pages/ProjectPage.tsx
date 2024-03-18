0e62432b66b6307097a71c7edba8050eed847deb
MachineLearning <AlphaBetaGo@163.com>
2024-03-18 16:03:45 +0800

    利用msw框架进行HTTP服务MOCK

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -1,12 +1,17 @@
 import {useState, useEffect} from 'react';
-import {Divider, Table, Input, Button} from "@douyinfe/semi-ui";
+import {Divider, Table, Input, Button, Form, Tooltip} from "@douyinfe/semi-ui";
 import {columns, data} from "../mocks/mockData.tsx";
+import {IconHelpCircle} from "@douyinfe/semi-icons";
 
 const ProjectPage = () => {
     // 筛选条件
     const [filter, setFilter] = useState('');
     // 数据
     const [tableData, setTableData] = useState(data);
+    // 标题
+    const [title, setTitle] = useState('');
+    // 作者
+    const [author, setAuthor] = useState('');
 
     // 监听筛选条件的变化
     useEffect(() => {
@@ -15,14 +20,11 @@ const ProjectPage = () => {
 
     // 提交按钮的点击事件处理函数
     const handleSubmit = async () => {
-
         try {
-            // 模拟向后端服务器发送请求并获取数据
-            // const response = await fetch('https://localhost:8080/data');
+            // 定义你要发送的数据
             const data = {
-                key1: 'value1',
-                key2: 'value2',
-                // ...其他参数
+                title: title,
+                author: author
             };
 
             // 发送POST请求
@@ -35,20 +37,31 @@ const ProjectPage = () => {
             });
 
             const newData = await response.json();
-            console.log(newData)
             setTableData(newData.concat(newData));
-            console.log(tableData);
         } catch (error) {
-            // setTableData(tableData);
+            // handle error
         }
     };
 
     return (
         <div>
             <Divider></Divider>
-            <Input value={filter} onChange={e => setFilter(e)} placeholder="Filter"/>
-            <Divider></Divider>
-            <Button onClick={handleSubmit}>Submit</Button>
+            <Form layout='horizontal' onValueChange={values=>console.log(values)}>
+                <Form.Input field='UserName' label='用户名' style={{ width: 80 }}/>
+                <Form.Input
+                    field='Password'
+                    label={{
+                        text: '密码',
+                        extra: <Tooltip content='详情11111'><IconHelpCircle style={{ color: 'var(--semi-color-text-2)' }}/></Tooltip>
+                    }}
+                    style={{ width: 176 }}
+                />
+                {/*<Form.Select field="Role" label={{ text: '角1111色', optional: true }} style={{ width: 176 }}>*/}
+                {/*    <Option value="admin">管理员</Option>*/}
+                {/*    <Option value="user">普通用户</Option>*/}
+                {/*    <Option value="guest">访客</Option>*/}
+                {/*</Form.Select>*/}
+            </Form>
             <Divider></Divider>
             <Table columns={columns} dataSource={tableData} pagination={false}/>
             <Divider></Divider>
